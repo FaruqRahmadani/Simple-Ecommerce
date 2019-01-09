aea09bc9bde98b878efe0cbc3ef6006ea32d8c7a
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-01-09 23:42:00 +0800

    hide navbar when not logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,10 @@
   <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
 </head>
 <body>
+  @if (Auth::User())
   <nav class="navbar navbar-expand-lg navbar-light bg-info">
     <div class="container">
       <a class="navbar-brand d-none d-md-block" href="#">Simple E-Commerce</a>
-      @if (Auth::User())
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -30,9 +30,9 @@
             </ul>
           </div>
         </div>
-      @endif
     </div>
   </nav>
+@endif
   <div class="content container mt-5">
     <div class="card">
       <div class="card-header">
