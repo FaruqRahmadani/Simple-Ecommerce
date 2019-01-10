2846cc93fbd6f5f8f1b678a14cc6cc836e7c6fb6
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-01-10 14:05:53 +0800

    old value when validator not pass

--- resources/views/prepaid/order.blade.php
+++ resources/views/prepaid/order.blade.php
@@ -7,7 +7,7 @@
       <div class="form-group row">
         <label class="col-md-2 col-form-label">Mobile Phone Number</label>
         <div class="col-md-10">
-          <input type="text" class="form-control" name="phone_number" placeholder="Mobile Phone Number" required>
+          <input type="text" class="form-control" name="phone_number" placeholder="Mobile Phone Number" value="{{old('phone_number')}}" required>
           @if ($errors->has('phone_number'))
             @foreach ($errors->get('phone_number') as $error)
               <p class="text-danger">{{$error}}</p>
@@ -20,9 +20,9 @@
         <div class="col-md-10">
           <select class="form-control" name="value" required>
             <option value="">Select Value</option>
-            <option value="10000">10.000</option>
-            <option value="50000">50.000</option>
-            <option value="100000">100.000</option>
+            <option value="10000" @if(old('value') == 10000) selected @endif>10.000</option>
+            <option value="50000" @if(old('value') == 50000) selected @endif>50.000</option>
+            <option value="100000" @if(old('value') == 100000) selected @endif>100.000</option>
           </select>
           @if ($errors->has('value'))
             @foreach ($errors->get('value') as $error)
